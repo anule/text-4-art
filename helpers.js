3312521c9a592c78789bf268fcd7b0b12390230e
anule <anule.ndukwu@gmail.com>
2020-04-04 20:56:07 -0700

    replace deprecated `request` module with `node-fetch`

--- helpers.js
+++ helpers.js
@@ -1,31 +1,37 @@
 // from AICbot by @backspace https://glitch.com/edit/#!/aicbot?path=helpers.js:1:0
 
 const fs = require('fs');
-const request = require('request');
+const fetch = require('node-fetch');
 
 module.exports = {
   get_random_object: function(cb) {
     let timeStamp = Math.floor(Date.now() / 1000);
     let artworkRequest = {
-      resources: 'artworks',
-      fields: ['id', 'title', 'artist_display', 'image_id', 'date_display', 'medium_display'],
-      boost: false,
-      limit: 1,
-      query: {
-        function_score: {
-          query: {
-            constant_score: {
-              filter: {
-                exists: {
-                  field: 'image_id',
+      "resources": "artworks",
+      "fields": [
+        "id",
+        "title",
+        "artist_display",
+        "image_id",
+        "date_display"
+      ],
+      "boost": false,
+      "limit": 1,
+      "query": {
+        "function_score": {
+          "query": {
+            "constant_score" : {
+              "filter": {
+                "exists": {
+                  "field": "image_id"
                 }
               }
             }
           },
-          boost_mode: 'replace',
-          random_score: {
-            field: 'id',
-            seed: timeStamp
+          "boost_mode": "replace",
+          "random_score": {
+            "field": "id",
+            "seed": timeStamp
           }
         }
       }
@@ -37,16 +43,16 @@ module.exports = {
       json: artworkRequest
     };
 
-    request.post(options, function(err, response, object) {
-      if (!err && object.status != 400) {
-        console.log('got an object');
-        console.log(object);
-        cb(object.data[0]);
-      } else {
-        console.log(err, 'error.', object);
-        cb(err, null);
-      }
-    });
+    fetch('https://aggregator-data.artic.edu/api/v1/search', {
+      method: 'POST',
+      body: JSON.stringify(artworkRequest),
+      headers: { 'Content-Type': 'application/json' }
+    }).then((res) => res.json())
+      .then(object => {
+        console.log('got an object', object)
+        cb(object.data[0])
+      })
+      .catch(e => console.log(e.statusCode, e.message));
   },
   load_image: function(url, cb) {
     console.log('loading remote image...');
--- server.js
+++ server.js
@@ -2,15 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const message = require('./message');
+const { get_random_object } = require('./helpers')
 
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send(message.string, message.image_url)
-  // res.send('Text 7732496838 for some art')
+  res.send('Text 7732496838 for some art')
 
 });
 
@@ -21,16 +20,26 @@ app.post('/sms', (req, res) => {
   const response = twiml.message();
 
   if (incoming.toLowerCase().includes('art')) {
-    response.body('You\'re getting some art!\n');
-    response.body(message.description);
-    response.media(message.image);
+    get_random_object(object => {
+      let string =
+        'Title: ' + object.title +
+        ', Artist: ' + object.artist_display +
+        ', Date: ' + object.date_display +
+        ', Medium: ' + object.medium_display;
+      response.body("You're getting some art!\n");
+      response.media('https://lakeimagesweb.artic.edu/iiif/2/' + object.image_id + '/full/1000,/0/default.jpg')
+      response.body(string)
+
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.send(response.toString());
+    });
   } else {
     response.body('no way! I only send art');
     response.media('https://media.giphy.com/media/l0MYSSCIrv8aUaBsQ/giphy.gif');
-  }
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(response.toString());
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.send(response.toString());
+  }
 });
 
 app.listen(3000, () => {
