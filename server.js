55f02686fdc13dce3859618246b17e3025e25482
anule <anule.ndukwu@gmail.com>
2020-04-05 17:57:14 -0700

    text-4-art only responds with art, when user sends a message with the word `art`

--- server.js
+++ server.js
@@ -14,30 +14,37 @@ app.get('/', (req, res) => {
 
 app.post('/sms', (req, res) => {
   console.log('message received...')
-  console.log('pinging AIC endpoint');
-  get_random_object().then(data => {
-    const twiml = new MessagingResponse();
-    const response = twiml.message();
-    console.log('got object, building message', data);
-    const image =
-      'https://lakeimagesweb.artic.edu/iiif/2/' +
-      data.image_id +
-      '/full/1000,/0/default.jpg';
-    const title = 'Title: ' + data.title;
-    const artist = 'Artist: ' + data.artist_display;
-    const date = 'Date: ' + data.date_display;
-    const medium = 'Medium: ' + data.medium_display;
+  const twiml = new MessagingResponse();
+  const message = twiml.message();
+  let textBody = req.body.Body.toLowerCase();
 
-    response.body(title);
-    response.body(artist);
-    response.body(date);
-    response.body(medium);
-    response.media(image);
-
+  if (!textBody.includes('art') && !textBody.includes('thank')) {
+    message.body('Hi! This is text-4-art 🖼, a service that brings the Art Institute of Chicago to you, one artwork at a time.\nTo receive a piece, please respond with a message that includes the word \'art\'.')
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
-  }).then(obj => console.log('done', obj))
-    .catch(e => console.error(e));
+  } else if (textBody.includes('thank') || textBody.includes('bye')) {
+    message.body('Thank you for visiting text-4-art. Visit us again soon!')
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  } else {
+    console.log('pinging AIC endpoint');
+    get_random_object().then(data => {
+      const twiml = new MessagingResponse();
+      const response = twiml.message();
+      console.log('got object, building message', data);
+      const image =
+        'https://lakeimagesweb.artic.edu/iiif/2/' +
+        data.image_id +
+        '/full/1000,/0/default.jpg';
+
+      response.body(`Title: ${data.title}\n\nArtist: ${data.artist_display}\n\nDate: ${data.date_display}\n\nMedium: ${data.medium_display}`);
+      response.media(image);
+
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.end(twiml.toString());
+    }).then(obj => console.log('done', obj))
+      .catch(e => console.error(e));
+  }
 });
 
 app.listen(3000, () => {
