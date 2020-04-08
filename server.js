e0a22448cf04c1947dc01aae59eee4ee8432df3e
anule <anule.ndukwu@gmail.com>
2020-04-07 20:47:17 -0700

    make text-4-art more conversational

--- server.js
+++ server.js
@@ -17,32 +17,67 @@ app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
   const message = twiml.message();
   let textBody = req.body.Body.toLowerCase();
+  let sendGratitudeKeywords = /thank|bye|no/g
+  let sendAppreciationKeywords = /nice|love|like/g
 
-  if (!textBody.includes('art') && !textBody.includes('thank') && !textBody.includes('loved') && !textBody.includes('liked')) {
-    message.body('Hi! This is text-4-art 🖼, a service that brings the Art Institute of Chicago to you, one artwork at a time.\nTo receive a piece, please respond with a message that includes the word \'art\'.')
+// like this until I can find an exclusionary regex
+  if ( // if the message includes this string...
+    // ...automatically send art
+    !textBody.includes('art') &&
+    !textBody.includes('yes') &&
+    !textBody.includes('more') &&
+
+    // ...send message of gratitude
+    !textBody.includes('thank') &&
+    !textBody.includes('bye') &&
+    !textBody.includes('no') &&
+
+    // ...send appreciation and ask if more is wanted
+    !textBody.includes('love') &&
+    !textBody.includes('like') &&
+    !textBody.includes('nice')
+  ) {
+    console.log('sending introduction...');
+    message.body(
+      "Hi! This is text-4-art 🖼, a service that brings the Art Institute of Chicago to you, one artwork at a time.\nTo receive a randomly selected piece, please respond with a message that includes the word 'art'."
+    );
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
-  } else if (textBody.includes('thank') || textBody.includes('bye')) {
-    message.body('Thank you for visiting text-4-art. Visit us again soon!')
+  } else if (textBody.search(sendGratitudeKeywords) >= 0) {
+    console.log('sending gratitute. we love our fans!');
+    message.body(
+      'Thank you for visiting text-4-art. We appreciate your patronage. Visit us again soon!'
+    );
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
+  } else if (textBody.search(sendAppreciationKeywords) >= 0) {
+    console.log('sending note of appreciation. asking if they want more...');
+    message.body(
+      "We're glad you enjoyed our selection. Would you like to receive another work of art?"
+    );
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
   } else {
     console.log('pinging AIC endpoint');
-    get_random_object().then(data => {
-      const twiml = new MessagingResponse();
-      const response = twiml.message();
-      console.log('got object, building message', data);
-      const image =
-        'https://lakeimagesweb.artic.edu/iiif/2/' +
-        data.image_id +
-        '/full/1000,/0/default.jpg';
+    get_random_object()
+      .then(data => {
+        const twiml = new MessagingResponse();
+        const response = twiml.message();
+        console.log('got object, building message', data);
+        const image =
+          'https://lakeimagesweb.artic.edu/iiif/2/' +
+          data.image_id +
+          '/full/1000,/0/default.jpg';
 
-      response.body(`Title: ${data.title}\n\nArtist: ${data.artist_display}\n\nDate: ${data.date_display}\n\nMedium: ${data.medium_display}\nwww.artic.edu/artworks/${data.id}`);
-      response.media(image);
+        response.body(
+          `Title: ${data.title}\n\nArtist: ${data.artist_display}\n\nDate: ${data.date_display}\n\nMedium: ${data.medium_display}\nwww.artic.edu/artworks/${data.id}`
+        );
+        response.media(image);
 
-      res.writeHead(200, { 'Content-Type': 'text/xml' });
-      res.end(twiml.toString());
-    }).then(obj => console.log('done', obj))
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end(twiml.toString());
+      })
+      .then(obj => console.log('done', obj))
       .catch(e => console.error(e));
   }
 });
